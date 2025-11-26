51f5e3b2b9da71abbc21b09f8c80bbd96d5aa7bc
AlexanderBezugliy <lrasel1992@gmail.com>
2025-11-26 21:11:35 +0200

    create section: Navbar & Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from './components/Navbar';
+import Hero from "./components/Hero";
 
 
 
@@ -9,9 +10,11 @@ gsap.registerPlugin(ScrollTrigger, SplitText); // нужно в ручную р�
 
 const App = () => {
     return (
-        <div className='flex-center h-[100vh]'>
-            <h1 className='text-3xl text-indigo-300'>GSAP</h1>
-        </div>
+        <main>
+            <Navbar />
+            <Hero />
+
+        </main>
     )
 }
 
